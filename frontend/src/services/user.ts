ece3ee22082868e295b22e0aa76f0a6ab6f813c5
Pintu <pintu.rana@sapiens.com>
2024-05-15 02:32:35 +0530

    chore: updated code

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = "http://services.idreambig.in/api";
 
 export const USER_API_END_POINT = "/users";
 export const FACULTY_API_END_POINT = "/users/faculty";
