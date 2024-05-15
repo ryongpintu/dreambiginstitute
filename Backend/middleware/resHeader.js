78d1de70b02ad71a7997acbef900755d88cf79cd
Pintu <pintu.rana@sapiens.com>
2024-05-16 01:29:06 +0530

    chore: udated backend code

--- Backend/middleware/resHeader.js
+++ Backend/middleware/resHeader.js
@@ -3,7 +3,7 @@
 module.exports=function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://services.idreambig.in');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
